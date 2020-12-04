5fc4d64ff032f68e1ee8e3cae6a672ec6bb149ce
Trangar <victor.koenders@gmail.com>
2020-12-04 10:44:29 +0100

    Made a day template and updated lib.rs

---
+++ src/bin/day_template.rs
@@ -0,0 +1,23 @@
+use aoc_2020::*;
+
+fn main() {
+    let input = input("dayxxx.txt");
+    // let input = input_isize("dayxxx.txt");
+
+    let start = std::time::Instant::now();
+    println!("Part 1:");
+    part1(&input);
+    println!("Done in {:?}", start.elapsed());
+
+    let start = std::time::Instant::now();
+    println!("Part 2:");
+    part2(&input);
+    println!("Done in {:?}", start.elapsed());
+}
+
+fn part1(input: &[String]) {
+
+}
+fn part2(input: &[String]) {
+
+}
--- src/lib.rs
+++ src/lib.rs
@@ -1,12 +1,21 @@
-pub fn input(file: &str) -> Vec<String> {
+fn contents(file: &str) -> String {
     use std::io::Read;
     let mut file = std::fs::File::open(format!("input/{}", file)).unwrap();
     let mut content = String::new();
     file.read_to_string(&mut content).unwrap();
 
-    content.split_whitespace().map(|s| s.to_owned()).collect()
+    content
+}
+
+pub fn input(file: &str) -> Vec<String> {
+    contents(file)
+        .split_whitespace()
+        .map(|s| s.to_owned())
+        .collect()
 }
 pub fn input_isize(file: &str) -> Vec<isize> {
-    let input = input(file);
-    input.into_iter().map(|i| i.parse().unwrap()).collect()
+    contents(file)
+        .split_whitespace()
+        .map(|i| i.parse().unwrap())
+        .collect()
 }
